146afe491f960456de17d5bd065ce350b8b4bbaa
Nikhil <nikhilagrawal6448@gmail.com>
2025-06-04 22:08:27 +0530

    update

--- main.py
+++ main.py
@@ -166,35 +166,35 @@ translator = Translator()
 
 @app.post("/english/")
 async def english(text: Text):
-    translation = await translator.translate(text.text, dest='en')
+    translation =  translator.translate(text.text, dest='en')
     return JSONResponse(content={"Translation": translation.text})
 
 @app.post("/hindi/")
 async def hindi(text: Text):
-    translation = await translator.translate(text.text, src="en", dest='hi')
+    translation =  translator.translate(text.text, src="en", dest='hi')
     return JSONResponse(content={"Translation": translation.text})
 
 @app.post("/punjabi/")
 async def punjabi(text: Text):
-    translation = await translator.translate(text.text, dest='pa')
+    translation =  translator.translate(text.text, dest='pa')
     return JSONResponse(content={"Translation": translation.text})
 
 @app.post("/gujarati/")
 async def gujarati(text: Text):
-    translation = await translator.translate(text.text, dest='gu')
+    translation =  translator.translate(text.text, dest='gu')
     return JSONResponse(content={"Translation": translation.text})
 
 @app.post("/bengali/")
 async def bengali(text: Text):
-    translation = await translator.translate(text.text, dest='bn')
+    translation =  translator.translate(text.text, dest='bn')
     return JSONResponse(content={"Translation": translation.text})
 
 @app.post("/tamil/")
 async def tamil(text: Text):
-    translation = await translator.translate(text.text, dest='ta')
+    translation =  translator.translate(text.text, dest='ta')
     return JSONResponse(content={"Translation": translation.text})
 
 @app.post("/telugu/")
 async def telugu(text: Text):
-    translation = await translator.translate(text.text, dest='te')
+    translation =  translator.translate(text.text, dest='te')
     return JSONResponse(content={"Translation": translation.text})
